67f0f77c838b0e71b824c621608a0ac83b09ea80
HP <HP@LAPTOP-2M2VELP9.bbrouter>
2025-07-24 20:00:11 +0530

    this is my day five single inheritance

---
+++ C2TC_CoreJavaPrograms/src/day5/singleinheritance/Singleinheritancedemo.java
@@ -0,0 +1,11 @@
+package day5.singleinheritance;
+
+public class Singleinheritancedemo {
+	public static void main(String[] args) {
+		
+		Student student = new Student("Raj", "20203040", "Mysore", 2288777123L, 8, "City College");
+		System.out.println(student);
+	}
+}
+
+
