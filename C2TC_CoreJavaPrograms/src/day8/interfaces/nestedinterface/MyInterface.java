23d37e74508c6d973cbe9ee2a96d4229221f234e
HP <HP@LAPTOP-2M2VELP9.bbrouter>
2025-07-29 20:56:58 +0530

    this is my nested interface program

---
+++ C2TC_CoreJavaPrograms/src/day8/interfaces/nestedinterface/MyInterface.java
@@ -0,0 +1,11 @@
+package day8.interfaces.nestedinterface;
+
+public interface MyInterface {
+	 void calculateArea();
+	    interface MyInnerInterface {
+	       int  id = 20;
+	       void print();     
+	    }
+
+
+}
