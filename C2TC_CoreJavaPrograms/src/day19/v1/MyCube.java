84e5268addd9daa72d8ac8a6d1ed2123c28f5ea8
HP <HP@LAPTOP-2M2VELP9.mshome.net>
2025-08-05 14:46:17 +0530

    this is my day19 programs

---
+++ C2TC_CoreJavaPrograms/src/day19/v1/MyCube.java
@@ -0,0 +1,9 @@
+package day19.v1;
+@FunctionalInterface
+
+public interface MyCube {
+	int getCube(int no);
+	//void show(); not allow more than one abstract method
+
+
+}
---
+++ C2TC_CoreJavaPrograms/src/day19/v1/Palindrome.java
@@ -0,0 +1,7 @@
+package day19.v1;
+@FunctionalInterface
+public interface Palindrome <T> {
+	boolean checkPalindrome(T data);
+
+
+}
